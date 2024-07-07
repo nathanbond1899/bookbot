788329285c6622ae7e9fad1f557c7554cbb20a92
nathanbond1899 <nathanbond1899@gmail.com>
2024-07-07 17:02:18 -0500

    Ignored /books. Added a function to read in txt file

---
+++ main.py
@@ -0,0 +1,12 @@
+with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+#open the franenstein.txt file and set it to file_contents
+
+def main():
+    print(file_contents)
+
+
+
+main()
+
+
