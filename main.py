767cc0903aea41697d97d8d667af0c53268b273d
nathanbond1899 <nathanbond1899@gmail.com>
2024-07-07 19:36:58 -0500

    Added dictionary for letter frequency

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    created_dict = get_count_characters(text)
+    print(created_dict)
 
 
 def get_num_words(text):
@@ -13,6 +15,19 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+
+def get_count_characters(text):
+    lower_text = text.lower()
+    count_dict = {}
+
+    for letter in lower_text:
+        if(letter in count_dict):
+            count_dict[letter] +=1
+        else:
+            count_dict[letter] = 1
+    #for key, value in count_dict.items():
+    return count_dict
 
 
 main()
