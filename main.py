18553cd1fa571e237187a82b1c25714f63d79b12
nathanbond1899 <nathanbond1899@gmail.com>
2024-07-07 19:14:26 -0500

    Cleaned up. Added count words function

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-#open the franenstein.txt file and set it to file_contents
-
 def main():
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
 
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 main()
-
-
